4c05b590a00f4fc56a62e6687060f24255631dbd
marmeladze <wrested@hotmail.de>
2017-04-04 11:41:08 +0400

    update place.rb

--- app/controllers/place.rb
+++ app/controllers/place.rb
@@ -1,23 +1,4 @@
-WebService::App.controllers :place, :provides => [:json] do
-  
-  # get :index, :map => '/foo/bar' do
-  #   session[:foo] = 'bar'
-  #   render 'index'
-  # end
-
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   "Maps to url '/foo/#{params[:id]}'"
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
+WebService::App.controllers :places, :provides => [:json] do
   get :index, :provides => :json do
     @places = Place.all.to_json
   end
@@ -27,6 +8,6 @@ WebService::App.controllers :place, :provides => [:json] do
   end
 
   get :search, :with => :name, :provides => :json do
-    @results = Place.where("name ILIKE ?", "%#{params[:name]}%").to_json
+    @results = Place.where("name LIKE ?", "%#{params[:name]}%").to_json
   end
 end
